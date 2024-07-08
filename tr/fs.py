01830444335a6aec6af6706e4d3ed4306bf96fa0
Михаил Капелько <kornerr@gmail.com>
2024-07-08 22:55:39 +0300

    d

---
+++ tr/fs.py
@@ -0,0 +1,40 @@
+import os
+from cld import *
+from fs_aux import *
+from tr_Context import *
+
+# Extract directory from config path
+#
+# Conditions:
+# 1. Config path has just been set
+@cld_by_value
+def fs_locateConfigDir(
+  c: tr_Context
+) -> tr_Context:
+    if (
+        c.recentField == "cfgPath"
+    ):
+        c.cfgDir = os.path.dirname(c.cfgPath)
+        c.recentField = "cfgDir"
+        return c
+
+    c.recentField = "none"
+    return c
+
+# Read config file contents
+#
+# Conditions:
+# 1. Config path has just been set
+@cld_by_value
+def fs_readConfig(
+  c: tr_Context
+) -> tr_Context:
+    if (
+        c.recentField == "cfgPath"
+    ):
+        c.cfgContents = fs_aux_readFile(c.cfgPath)
+        c.recentField = "cfgContents"
+        return c
+
+    c.recentField = "none"
+    return c
--- tr/tr_Context.py
+++ tr/tr_Context.py
@@ -2,6 +2,13 @@ from cld import *
 
 class tr_Context:
     def __init__(self):
-        self.cfgContents = None
+        self.cfgContents = []
+        self.cfgDir = None
         self.cfgPath = None
-        self.scriptPath = None
+        self.scriptDir = None
+
+    def field(self, fieldName):
+        return getattr(self, fieldName)
+
+    def setField(self, fieldName, value):
+        setattr(self, fieldName, value)
