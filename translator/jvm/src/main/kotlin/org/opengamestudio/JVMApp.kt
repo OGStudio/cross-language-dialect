f4603bf76db5c5f6fa3c38a3aaf44ef8e8e62a32
Михаил Капелько <kornerr@gmail.com>
2024-12-23 22:27:40 +0300

    d

--- translator/jvm/src/main/kotlin/org/opengamestudio/JVMApp.kt
+++ translator/jvm/src/main/kotlin/org/opengamestudio/JVMApp.kt
@@ -4,13 +4,11 @@ class JVMApp { }
 
 fun main(args: Array<String>) {
     println("Hello, World, this is translator stub")
-    /*
     val ctrl = CLDController(Context())
-    trgRegisterCommonPlatformFunctions(p)
-    trgRegisterCommonShoulds(ctrl)
-    trgRegisterCommonSettings(ctrl)
+    //trgRegisterCommonPlatformFunctions(p)
+    //trgRegisterCommonShoulds(ctrl)
+    //trgRegisterCommonSettings(ctrl)
 
     ctrl.set("arguments", args)
     ctrl.set("didLaunch", true)
-    */
 }
---
+++ translator/src/Context.kt
@@ -0,0 +1,46 @@
+package org.opengamestudio
+
+// Application state
+data class Context(
+    // Command line arguments
+    var arguments: Array<String> = arrayOf(),
+    // String to print to console
+    var consoleOutput: String = "",
+    // The application did finish launching
+    var didLaunch: Boolean = false,
+    // Path to input file
+    var pathInput: String = "",
+    override var recentField: String = "",
+): CLDContext {
+    override fun <T> field(name: String): T {
+        if (name == "arguments") {
+            return arguments as T
+        } else if (name == "consoleOutput") {
+            return consoleOutput as T
+        } else if (name == "didLaunch") {
+            return didLaunch as T
+        } else if (name == "pathInput") {
+            return pathInput as T
+        }
+        return "unknown-field-name" as T
+    }
+
+    override fun selfCopy(): CLDContext {
+        return this.copy()
+    }
+
+    override fun setField(
+        name: String,
+        value: Any?
+    ) {
+        if (name == "arguments") {
+            arguments = value as Array<String>
+        } else if (name == "consoleOutput") {
+            consoleOutput = value as String
+        } else if (name == "didLaunch") {
+            didLaunch = value as Boolean
+        } else if (name == "pathInput") {
+            pathInput = value as String
+        }
+    }
+}
