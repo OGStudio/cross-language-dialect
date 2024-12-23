0cbc79d5007e0e7a50ba75c157b0370ad9a3210b
Михаил Капелько <kornerr@gmail.com>
2024-12-23 22:12:48 +0300

    d

---
+++ translator/jvm/src/main/kotlin/org/opengamestudio/JVMApp.kt
@@ -0,0 +1,16 @@
+package org.opengamestudio
+
+class JVMApp { }
+
+fun main(args: Array<String>) {
+    println("Hello, World, this is translator stub")
+    /*
+    val ctrl = CLDController(Context())
+    trgRegisterCommonPlatformFunctions(p)
+    trgRegisterCommonShoulds(ctrl)
+    trgRegisterCommonSettings(ctrl)
+
+    ctrl.set("arguments", args)
+    ctrl.set("didLaunch", true)
+    */
+}
