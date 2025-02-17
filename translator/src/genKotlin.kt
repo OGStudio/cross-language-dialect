b2fd1ba1c98bf556e1d64609a202b6f72f15ce24
Михаил Капелько <kornerr@gmail.com>
2025-02-17 22:40:15 +0300

    d

--- translator/src/genKotlin.kt
+++ translator/src/genKotlin.kt
@@ -1,11 +1,19 @@
 package org.opengamestudio
 
-// Generate Kotlin representation of entities in single file
+// Generate Kotlin comment based on its emptiness
+fun genKotlinComment(comment: String): String {
+    if (!comment.isEmpty()) {
+        return PREFIX_KOTLIN_COMMENT + comment
+    }
+    return ""
+}
+
+// Generate Kotlin representation of all YAML entities in a single file
 fun genKotlinEntitiesFile(
     entityNames: Array<String>,
     entityComments: Map<Int, String>
 ): String {
-    var s = "TODO: real generation "
+    var s = ""
     // Entity by id.
     var id = 0
     for (entityName in entityNames) {
@@ -17,11 +25,13 @@ fun genKotlinEntitiesFile(
     return s
 }
 
-// Generate Kotlin representation of a single entities
+// Generate Kotlin representation of a single entity
 fun genKotlinEntity(
     name: String,
     comment: String
 ): String {
+    val genComment = genKotlinComment(comment)
     return TEMPLATE_KOTLIN_STRUCT
         .replace("%NAME%", name)
+        .replace("%COMMENT%", genComment)
 }
