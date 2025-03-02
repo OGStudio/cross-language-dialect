f2e8c6eb49e9c6b2ef22ecb73b3800dd5aecbcc3
Михаил Капелько <kornerr@gmail.com>
2025-03-02 23:16:48 +0300

    d

--- translator/src/genKotlin.kt
+++ translator/src/genKotlin.kt
@@ -156,7 +156,7 @@ fun genKotlinFieldType(type: String): String {
     ) {
         val innerString = type.substring(1, type.length - 1)
         val parts = innerString.split(DICTIONARY_DELIMITER)
-        /**/print("ИГР genKFT-01 parts: '${dbgStringArray(parts.toTypedArray())}'")
+        /**/print("ИГР genKFT-01 parts-1/2: '${parts.first()}'/'${parts.last()}'")
         // Recursive calls to format types
         val innerTypeA = genKotlinFieldType(parts.first())
         val innerTypeB = genKotlinFieldType(parts.last())
