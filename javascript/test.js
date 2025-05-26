8bfba62ba9b71e338690f8b4e2969a30d616bf88
Михаил Капелько <kornerr@gmail.com>
2025-05-26 22:15:34 +0300

    example context for tests

---
+++ javascript/test.js
@@ -0,0 +1,40 @@
+
+/// Sample Context used for testing
+function ExampleContext() {
+    this._construct = function() {
+        this.didLaunch = false;
+        this.host = "";
+        this.sometimes = "";
+
+        this.recentField = "";
+    };
+    this._construct();
+
+    this.field = function(name) {
+        if (name == "didLaunch") {
+            return this.didLaunch;
+        } else if (name == "host") {
+            return this.host;
+        } else if (name == "sometimes") {
+            return this.sometimes;
+        }
+
+        return "unknown-field-name";
+    };
+
+    this.selfCopy = function() {
+        let that = structuredCopy(this);
+        return that;
+    };
+
+    this.setField = function(name, value) {
+        if (name == "didLaunch") {
+            this.didLaunch = value;
+        } else if (name == "host") {
+            this.host = value;
+        } else if (name == "sometimes") {
+            this.sometimes = value;
+        }
+    };
+}
+
