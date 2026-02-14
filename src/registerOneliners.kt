f50c43d3269dd048ec4c7522bde7606569d3157b
Михаил Капелько <kornerr@gmail.com>
2026-02-14 22:04:17 +0300

    reg onel

---
+++ src/registerOneliners.kt
@@ -0,0 +1,21 @@
+/**
+ * This file is a part of Kotlin dialect:
+ *     https://github.com/OGStudio/kotlin-dialect
+ * License: CC0
+ * Version: 2.0.0
+ */
+
+package org.opengamestudio
+
+// Register several oneliner callbacks to a controller
+fun registerOneliners(
+    ctrl: KDController,
+    items: Array<Any>
+) {
+    val halfCount = items.size / 2
+    for (i in 0..<halfCount) {
+        val field = items[i * 2] as String
+        val callback = items[i * 2 + 1] as (c: KDContext) -> Unit
+        ctrl.registerFieldCallback(field, callback);
+    }
+}
