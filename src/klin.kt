890e9955defe386b40cfa0af256850a752fbc5e7
Михаил Капелько <kornerr@gmail.com>
2026-02-20 21:37:27 +0300

    rm isDbg

--- src/klin.kt
+++ src/klin.kt
@@ -21,7 +21,6 @@ data class AppContext(
     var entityTypes: Map<Int, String> = mapOf(),
     var inputFile: String = "",
     var inputFileLines: Array<String> = arrayOf(),
-    var isDbg: Boolean = false,
     var outputFile: String = "",
     // Contents to write to output file
     var outputFileContents: String = "",
@@ -54,8 +53,6 @@ data class AppContext(
             return inputFile as T
         } else if (name == "inputFileLines") {
             return inputFileLines as T
-        } else if (name == "isDbg") {
-            return isDbg as T
         } else if (name == "outputFile") {
             return outputFile as T
         } else if (name == "outputFileContents") {
@@ -98,8 +95,6 @@ data class AppContext(
             inputFile = value as String
         } else if (name == "inputFileLines") {
             inputFileLines = value as Array<String>
-        } else if (name == "isDbg") {
-            isDbg = value as Boolean
         } else if (name == "outputFile") {
             outputFile = value as String
         } else if (name == "outputFileContents") {
